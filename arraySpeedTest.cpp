b3c5b77f0b237c99e76b4ec751866aef58d41114
Kyle Dukart <46544456+kyledukart@users.noreply.github.com>
2021-05-12 16:18:32 -0700

    initializating array and vector

--- arraySpeedTest.cpp
+++ arraySpeedTest.cpp
@@ -1,1 +1,44 @@
 // array speed test
+
+#include <iostream>
+#include <chrono>
+#include <vector>
+
+using namespace std;
+
+typedef chrono::high_resolution_clock Clock;
+
+int number_of_elements = 100000;
+
+int* theArray;
+vector<int> theVector;
+
+int main() {
+
+  //initialize
+  auto arrayStart = Clock::now();
+  testArrayInitialize();
+  auto arrayEnd = Clock::now();
+  cout << "Array initialization took "
+    << chrono::duration_cast<std::chrono::microseconds>(arrayEnd - arrayStart).count() << "µs";
+
+  auto vectorStart = Clock::now();
+  testVectorInitialize();
+  auto vectorEnd = Clock::now();
+  cout << "Array initialization took "
+    << chrono::duration_cast<std::chrono::microseconds>(arrayEnd - arrayStart).count() << "µs";
+
+}
+
+void testArrayInitialize() {
+  theArray = new int[number_of_elements];
+  for (unsigned int i = 0; i < number_of_elements; ++i)
+    theArray[i] = i;
+}
+
+void testVectorInitialize() {
+  theVector.reserve(number_of_elements);
+  for (unsigned int i = 0; i < number_of_elements; ++i)
+    theVector.push_back(i);
+}
+
